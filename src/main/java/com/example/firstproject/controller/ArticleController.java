98c30c3fdc94d369dee7c29fbad235d823d977ef
yang dong won <didehddnjs89@gmail.com>
2025-08-14 15:30:26 +0900

    lombok 추가, articleController show function 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,16 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j // logging
 public class ArticleController {
     @Autowired // sprint boot가 생성해놓은 객체 자동 연결
     private ArticleRepository articleRepository;
@@ -20,14 +24,23 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm articleForm){
-        System.out.println(articleForm.toString());
-
         Article article = articleForm.toEntity();
 
         // repository에게 entity를 db 안에 저장하도록 함.
         Article saved = articleRepository.save(article);
-        System.out.println(saved);
+        log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        model.addAttribute("article", articleEntity);
+
+        return "articles/show";
     }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Article {
-
     @Id
     @GeneratedValue // 1, 2, 3 ... 자동으로 생성
     private Long id;
@@ -15,22 +20,4 @@ public class Article {
     private String title;
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    protected Article() {
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
