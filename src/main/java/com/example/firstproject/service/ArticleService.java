847cbcdd12ddd5afb15c3bc8b3d0ac4412ab6d89
yang dong won <didehddnjs89@gmail.com>
2025-08-27 14:30:48 +0900

    test code 작성

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -5,11 +5,9 @@ import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
--- src/test/java/com/example/firstproject/FirstprojectApplicationTests.java
+++ src/test/java/com/example/firstproject/FirstprojectApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FirstprojectApplicationTests {
 
+
 	@Test
 	void contextLoads() {
 	}
