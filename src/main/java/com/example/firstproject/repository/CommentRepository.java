6d4df6ce702cb8ef0a6c8bd4991bc34e6b7cc6f9
yang dong won <didehddnjs89@gmail.com>
2025-08-28 18:19:03 +0900

    comment entity, repository 생성

---
+++ src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Comment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+    @Query(value =
+            "SELECT * " +
+                    "FROM comment " +
+                    "WHERE article_id = :articleId",
+            nativeQuery = true)
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
+
+    List<Comment> findByNickname(String nickname);
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,21 @@
 INSERT INTO article(title, content) VALUES('가가가가', '1111');
 INSERT INTO article(title, content) VALUES('나나나나', '2222');
 INSERT INTO article(title, content) VALUES('다다다다', '3333');
+
+-- 22강: article dummy data
+INSERT INTO article(title, content) VALUES('What is your life movie?', 'Leave a comment');
+INSERT INTO article(title, content) VALUES('What is your soul food?', 'Leave a comment');
+INSERT INTO article(title, content) VALUES('What is your hobby?', 'Leave a comment');
+-- 22강: comment dummy data
+---- Comments for article 4
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Park', 'Good Will Hunting');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Kim', 'I Am Sam');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Choi', 'The Shawshank Redemption');
+---- Comments for article 5
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Park', 'Chicken');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Kim', 'Shabu-shabu');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Choi', 'Sushi');
+---- Comments for article 6
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Park', 'Jogging');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Kim', 'YouTube');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Choi', 'Reading');
